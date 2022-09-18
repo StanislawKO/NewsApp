18db80f0c2eca2648c9be9680095b9e5d3ff65b1
Stanislav Kozh <printks@yandex.ru>
2022-09-18 22:38:19 +0300

    👉 Fix the Splash screen logic 👉 Create a DAO for the article 👉 Create a local database 👉 Setting up DI Halt 👉 Output the response to the console

--- app/src/main/java/com/codexample/newsapp/data/api/NewsService.kt
+++ app/src/main/java/com/codexample/newsapp/data/api/NewsService.kt
@@ -1,6 +1,8 @@
 package com.codexample.newsapp.data.api
 
+import com.codexample.newsapp.models.NewsResponse
 import com.codexample.newsapp.utils.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,11 +13,12 @@ interface NewsService {
         @Query("q") query: String,
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ): Response<NewsResponse>
 
+    @GET("/v2/top-headlines")
     suspend fun getHeadlines(
         @Query("country") countryCode: String = "ru",
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKe: String = API_KEY
-    )
+    ): Response<NewsResponse>
 }

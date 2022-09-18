3142ea7a2e40f585e808fe0cd8346088fae27170
Stanislav Kozh <printks@yandex.ru>
2022-09-18 21:10:36 +0300

    👉 Get the API Key 👉 Let's create models 👉 Creating an interface with queries 👉 Setting up Retrofit

---
+++ app/src/main/java/com/codexample/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.codexample.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "7bec4d9def724802be3bf0d8f118cb12"
+    }
+}
